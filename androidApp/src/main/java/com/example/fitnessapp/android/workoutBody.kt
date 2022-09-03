dacec71ddaf02a1a4ded91de81f38de148b35bf5
meizhongg <yeomeizhong@hotmail.com>
2022-09-04 03:11:19 +0800

    update

--- androidApp/src/main/java/com/example/fitnessapp/android/workoutBody.kt
+++ androidApp/src/main/java/com/example/fitnessapp/android/workoutBody.kt
@@ -1,14 +1,17 @@
 package com.example.fitnessapp.android
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.MediaController
+import android.widget.TextView
 import android.widget.VideoView
 
 class workoutBody : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workout_body)
@@ -19,17 +22,39 @@ class workoutBody : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val video = findViewById<VideoView>(R.id.Video)
-        val mediaController = MediaController(this)
-        mediaController.setAnchorView(video)
-        val uri: Uri =
-            Uri.parse("android.resource://com.example.fitnessapp.android/" + R.raw.benchpress)
-        video.setMediaController(mediaController)
-        video.setVideoURI(uri)
-
-        video.setOnClickListener {
-            video.requestFocus()
-            video.start()
+        val squats = findViewById<TextView>(R.id.WorkoutDescription1)
+        squats.text = getString(R.string.workout_squats) +
+                System.getProperty("line.separator") +
+                getString(R.string._4_sets_10_reps)
+        squats.setOnClickListener {
+            val intent = Intent(this, com.example.fitnessapp.android.squats::class.java)
+            startActivity(intent)
         }
+
+        val bench = findViewById<TextView>(R.id.WorkoutDescription2)
+        bench.text = getString(R.string.workout_benchpress) +
+                System.getProperty("line.separator") +
+                getString(R.string._3_sets_10_reps)
+        bench.setOnClickListener {
+            val intent = Intent(this, benchpress::class.java)
+            startActivity(intent)
+        }
+
+        val pullup = findViewById<TextView>(R.id.WorkoutDescription3)
+        pullup.text = getString(R.string.workout_pullups) +
+                System.getProperty("line.separator") +
+                getString(R.string._3_sets_10_reps)
+
+        val shoulderPress = findViewById<TextView>(R.id.WorkoutDescription4)
+        shoulderPress.text = getString(R.string.workout_shoulderPress) +
+                System.getProperty("line.separator") +
+                getString(R.string._3_sets_8_reps) +
+                " " +
+                getString(R.string.double_dropset)
+        shoulderPress.setOnClickListener {
+            val intent = Intent(this, benchpress::class.java)
+            startActivity(intent)
+        }
+
     }
 }
